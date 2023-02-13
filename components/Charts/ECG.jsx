4098b96cf8039842cdca206cf82230f63c142e62
Danyal Muneer <abbasidani82@gmail.com>
2023-02-13 15:01:00 +0500

    updating line chart

--- components/Charts/ECG.jsx
+++ components/Charts/ECG.jsx
@@ -4,10 +4,24 @@ import { View } from "react-native";
 
 class EcgChart extends React.PureComponent {
   render() {
-    const data = [
-      50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80,
+   
+    const labelsData=[0,1,2,3,4,5,6]
+    const LineData = [
+      { x: 0, y: 1.1 },
+      { x: 1, y: 1 },
+      { x: 2, y: 2.5 },
+      { x: 3, y: -1.5 },
+      { x: 4, y: 0 },
+      { x: 5, y: -2.5 },
+      { x: 6, y: 2.5 },
     ];
-
+    const getLineData=(linevalue)=>{
+const newLienArray=[];
+linevalue?.map(item=>{
+  newLienArray.push(item?.y)
+})
+return newLienArray;
+    }
     const axesSvg = { fontSize: 10, fill: "gray" };
     const verticalContentInset = { top: 10, bottom: 10 };
     const xAxisHeight = 30;
@@ -37,7 +51,7 @@ const results={
     return (
       <View style={{ height: 200, padding: 20, flexDirection: "row" }}>
         <YAxis
-          data={[-2.5,-2,-1.5,-1,0,1,2,2.5]}
+          data={[-2.5, -2, -1.5, -1, 0, 1, 2, 2.5]}
           style={{ marginBottom: xAxisHeight }}
           contentInset={verticalContentInset}
           svg={axesSvg}
@@ -45,15 +59,15 @@ const results={
         <View style={{ flex: 1, marginLeft: 10 }}>
           <LineChart
             style={{ flex: 1 }}
-            data={data}
+            data={getLineData(LineData)}
             contentInset={verticalContentInset}
             svg={{ stroke: "blue" }}
           >
-            <Grid  />
+            <Grid />
           </LineChart>
           <XAxis
             style={{ marginHorizontal: -10, height: xAxisHeight }}
-            data={data}
+            data={labelsData}
             formatLabel={(value, index) => index}
             contentInset={{ left: 10, right: 10 }}
             svg={axesSvg}
